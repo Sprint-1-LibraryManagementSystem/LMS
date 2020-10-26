2f7edce3151640c9a972ed52cc593f94ae74aefc
Battugowthami <72733827+Battugowthami@users.noreply.github.com>
2020-10-26 13:56:20 +0530

    Create AuthorNotFoundException.java

---
+++ AuthorNotFoundException.java
@@ -0,0 +1,10 @@
+package com.capgemini.lms.exception;
+
+public class AuthorNotFoundException extends Exception{
+	public AuthorNotFoundException(String msg) {
+		super(msg);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
