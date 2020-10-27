7c64765ce4e11b4ad5732b1cbe5fe0ff5b56d4e3
jayasreepandi <72733456+jayasreepandi@users.noreply.github.com>
2020-10-27 09:38:16 +0530

    Create OrderNotFoundException.java

---
+++ OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.lms.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public OrderNotFoundException(String message) {
+		super(message);
+	}
+
+}
